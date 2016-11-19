1660cb1d7174b2be4bdb5ce23010ee5515295cf9
Drew Hugill <drew.hugill@gmail.com>
2016-11-18 19:41:24 -0500

    second commit

---
+++ Project 2/Assets/Scripts/Cube.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+    public Material[] materials;
+    public float rotationSpeed;
+    Renderer rend;
+    Shader shade;
+
+	// Use this for initialization
+	void Start () {
+        rotationSpeed = 50f;
+        shade = GetComponent<Shader>();
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        
+        //rend.sharedMaterial = materials[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            rend.sharedMaterial = materials[1];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            rend.sharedMaterial = materials[2];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            rend.sharedMaterial = materials[3];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            rend.sharedMaterial = materials[4];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            rend.sharedMaterial = materials[5];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha6))
+        {
+            rend.sharedMaterial = materials[6];
+        }
+    }
+}
---
+++ Project 2/Assets/Scripts/camera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+    public bool ortho;
+
+	// Use this for initialization
+	void Start () {
+        ortho = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if(ortho)
+            {
+                ortho = false;
+            }
+            else
+            {
+                ortho = true;
+            }
+            
+            
+        }
+        Camera.main.orthographic = ortho;
+    }
+}
